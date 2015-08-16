2ad3eb925809dfeb22d610fdd4d272400fb7ef18
Andrei Vidrasco <andrei.vidrasco@icloud.com>
2015-08-16 22:57:27 +0300

    Disable auto-respolve for shadow play

--- World/AppDelegate.m
+++ World/AppDelegate.m
@@ -17,14 +17,14 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:ShadowCharacterWanda];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:ShadowCharacterJustacreep];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:3];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:5];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:6];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:7];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:8];
-    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:1];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:ShadowCharacterWanda];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:ShadowCharacterJustacreep];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:3];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:5];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:6];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:7];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:8];
+//    [[ShadowPlayOpenedHandler sharedHandler] markAsOpenedCharacter:1];
  
     return YES;
 }
